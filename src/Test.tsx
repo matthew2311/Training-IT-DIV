e9506b46cd90bc3f4b962e4202adc7cf4213cee9
Matthew Sebastian <matthew.sebastian@binus.edu>
2023-01-17 11:31:21 +0700

    first commit

---
+++ src/Test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Test() {
+  return (
+    <div className="App">
+        <h1>Training IT DIV</h1>
+    </div>
+  );
+}
+
+export default Test;
